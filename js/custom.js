5c314c0317c36188d4894cd280afc3063f47e053
Andreas H <andi@blafasl.de>
2015-10-13 00:07:28 +0200

    add smooth scrolling and change some text

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,22 @@ $(document).ready(function () {
     });
 });
 
+// Add smooth scrolling to all links inside a navbar
+$("#main-nav-top a").on('click', function(event){
+
+  // Prevent default anchor click behavior
+  event.preventDefault();
+
+  // Store hash (#)
+  var hash = this.hash;
+
+  // Using jQuery's animate() method to add smooth page scroll
+  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 800, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+  });
+});
